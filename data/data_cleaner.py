e4ca8970757d5ccd0fc74209d80f80469d455c27
Blake Haugen <haugen.blake@gmail.com>
2013-11-26 16:56:41 -0500

    added segment data set

--- data/data_cleaner.py
+++ data/data_cleaner.py
@@ -15,7 +15,7 @@ for year in years:
 file_prefix="raw_data/TOP500_"
 file_suffix=".csv"
 master_data=pd.DataFrame() 
-
+perf_data={}
 def main():
     master_data=pd.DataFrame() 
     variables=collections.defaultdict(list)
@@ -47,6 +47,79 @@ def main():
     print master_data
     master_data.to_csv('clean_data.csv')
 
+    for index, row in master_data.iterrows():
+        listid=row['ListID']
+        if listid in perf_data.keys():
+            perf_data[listid]['top']=max(perf_data[listid]['top'],row['RMax'])
+            perf_data[listid]['bottom']=min(perf_data[listid]['bottom'],row['RMax'])
+            perf_data[listid]['total']=perf_data[listid]['total']+row['RMax']
+        else:
+            perf_data[listid]={ 'top':row['RMax'],
+                                'bottom':row['RMax'],
+                                'total':row['RMax']}
+    
+    #perf_data_df=pd.DataFrame(columns=('listid','top','bottom','total'))
+    perf_data_list=[]
+    for key in lists:
+        if key in perf_data:
+            entry=perf_data[key]
+            perf_data_list.append({'date':key,'top':entry['top'],'bottom':entry['bottom'],'total':entry['total']})
+    perf_data_df=pd.DataFrame(perf_data_list)
+    print perf_data_df
+    perf_data_df.to_csv('perf_data.csv')
 
 
+    core_data={}
+    for index, row in master_data.iterrows():
+        listid=row['ListID']
+        if listid in core_data:
+            core_data[listid]['max']=max(core_data[listid]['max'],row['Total Cores'])
+            core_data[listid]['min']=min(core_data[listid]['min'],row['Total Cores'])
+            core_data[listid]['mean']=core_data[listid]['mean']+row['Total Cores']
+            if row['Rank']==1:
+                core_data[listid]['#1']=row['Total Cores']
+
+        else:
+            if row['Rank']==1:
+                core_data[listid]={'max':row['Total Cores'],
+                                    'min':row['Total Cores'],
+                                    'mean':row['Total Cores'],
+                                    '#1':row['Total Cores']
+                                    }
+            else:
+                core_data[listid]={ 'max':row['Total Cores'],
+                                    'min':row['Total Cores'],
+                                    'mean':row['Total Cores'],
+                                    '#1':None
+                                    }
+
+    core_data_list=[]
+    for key in lists:
+        if key in core_data:
+            entry=core_data[key]
+            core_data_list.append({'date':key,'max':entry['max'],'min':entry['min'],'mean':entry['mean']/500.0,'#1':entry['#1']})
+    core_data_df=pd.DataFrame(core_data_list)
+    print core_data_df
+    core_data_df.to_csv('core_data.csv')
+
+    segment_data={}
+    for index,row in master_data.iterrows():
+        listid = row['ListID']
+        segment=row['Segment']
+        if listid in segment_data:
+            if segment in segment_data[listid]:
+                segment_data[listid][segment]+=1
+            else:
+                segment_data[listid][segment]=1
+        else:
+            segment_data[listid]={"date":listid,segment:1}
+    print segment_data
+    segment_list=[]
+    for key in segment_data:
+        segment_list.append(segment_data[key])
+    segment_df=pd.DataFrame(segment_list)
+    print segment_df
+    segment_df.to_csv("segment_data.csv")
+
+        
 main()
